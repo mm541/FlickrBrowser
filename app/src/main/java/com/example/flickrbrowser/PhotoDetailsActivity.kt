73f398161086b555528f5af204b8e050a2e891c1
mm541145 <moazzamparwez40@gmail.com>
2024-01-03 16:05:45 +0530

    just some minor changes

--- app/src/main/java/com/example/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/com/example/flickrbrowser/PhotoDetailsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.flickrbrowser
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -18,12 +19,16 @@ class PhotoDetailsActivity : AppCompatActivity() {
         setSupportActionBar(toolbarPhoto)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+        val photo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra(PHOTO_TRANSFER,Photo::class.java)!!
+        } else {
+            intent.getParcelableExtra(PHOTO_TRANSFER)
+        }
 
-        _binding.photoAuthor.text = photo.author
-        _binding.photoTitle.text = photo.title
-        _binding.photoTags.text = photo.tags
-        Log.d("photoDetailActivity", photo.link)
+        _binding.photoAuthor.text = photo?.author
+        _binding.photoTitle.text = photo?.title
+        _binding.photoTags.text = photo?.tags
+        Log.d("photoDetailActivity", photo!!.link)
         Log.d("photoDetailActivity", photo.image)
         Picasso.get().load(photo.link).error(R.drawable.placeholder)
             .placeholder(R.drawable.placeholder)
